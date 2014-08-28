b26aed817b58f1f469c0ff0675544958bc47f016
Pablo Felipe Aviles Ruiz <pfa217@lehigh.edu>
2014-08-28 17:23:23 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+// Pablo Aviles
+// Lab 1
+// Hello World Java Program
+
+// Define a class
+public class HelloWorld {
+    // define a method
+    public static void main (String[] args) {
+        System.out.println("#################");
+        System.out.println("#################");
+        System.out.println("#               #");
+        System.out.println("#  Hello World  #");
+        System.out.println("#               #");
+        System.out.println("#################");
+        System.out.println("#################");
+        System.out.println("test");
+    }
+}
