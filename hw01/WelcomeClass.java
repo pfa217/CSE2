d7c23c334112180255089246b8c630585d4f5647
Pablo Felipe Aviles Ruiz <pfa217@lehigh.edu>
2014-08-29 13:59:15 +0000

    Updated welcome class

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+//// Pablo Aviles
+// Homework 1
+// Welcome Class
+
+// Class definition
+public class WelcomeClass{
+    
+    // Main method
+    public static void main(String[] args){
+        System.out.println("   -----------");
+        System.out.println("   | Welcome |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-P--F--A--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("A year ago I studied in Europe for two months. I was living in Florence and I learned Italian");
+    }
+}
