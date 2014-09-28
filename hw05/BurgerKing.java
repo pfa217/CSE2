273a2f0030fbc23b44d8331a79957e9bb150ec78
Pablo Felipe Aviles Ruiz <pfa217@lehigh.edu>
2014-09-28 17:18:03 +0000

    Updated BurgerKing

--- hw05/BurgerKing.java
+++ hw05/BurgerKing.java
@@ -43,162 +43,78 @@ public class BurgerKing {
             switch (ch) {
                 // Choice of burger
                 case 'B':
-                    System.out.println(burger);
-                    burgerChoice = scanner.next();
-                    if (!(burgerChoice.length() > 1)) {
-                        switch (burgerChoice.charAt(0)) {
-                            case 'A':
-                                System.out.println("You orderar a burger with all the fixins");
-                                break;
-                            case 'a':
-                                System.out.println("You orderar a burger with all the fixins");
-                                break;
-                            case 'C':
-                                System.out.println("You ordered a burger with cheese");
-                                break;
-                            case 'c':
-                                System.out.println("You ordered a burger with cheese");
-                                break;
-                        }
-                    }
-                    break;
                 case 'b':
                     System.out.println(burger);
                     burgerChoice = scanner.next();
                     if (!(burgerChoice.length() > 1)) {
                         switch (burgerChoice.charAt(0)) {
                             case 'A':
-                                System.out.println("You orderar a burger with all the fixins");
-                                break;
                             case 'a':
                                 System.out.println("You orderar a burger with all the fixins");
                                 break;
+
                             case 'C':
-                                System.out.println("You ordered a burger with cheese");
-                                break;
                             case 'c':
                                 System.out.println("You ordered a burger with cheese");
                                 break;
+                            case 'N':
+                            case 'n':
+                                System.out.println("You ordered a plain burger");
+                                break;
+                                
                         }
                     }
                     break;
                 // Choice of soda
                 case 'S':
-                    System.out.println(soda);
-                    sodaChoice = scanner.next();
-                    if (!(sodaChoice.length() > 1)) {
-                        switch (sodaChoice.charAt(0)) {
-                            case 'P':
-                                System.out.println("You ordered Pepsi");
-                                break;
-                            case 'p':
-                                System.out.println("You ordered Pepsi");
-                                break;
-                            case 'C':
-                                System.out.println("You ordered Coke");
-                                break;
-                            case 'c':
-                                System.out.println("You ordered Coke");
-                                break;
-                            case 'S':
-                                System.out.println("You ordered Sprite");
-                                break;
-                            case 's':
-                                System.out.println("You ordered Sprite");
-                                break;
-                            case 'M':
-                                System.out.println("You ordered Mountain Dew");
-                                break;
-                            case 'm':
-                                System.out.println("You ordered Mountain Dew");
-                                break;
-                        }
-                    }
-                    break;
-
                 case 's':
                     System.out.println(soda);
                     sodaChoice = scanner.next();
                     if (!(sodaChoice.length() > 1)) {
                         switch (sodaChoice.charAt(0)) {
                             case 'P':
-                                System.out.println("You ordered Pepsi");
-                                break;
                             case 'p':
                                 System.out.println("You ordered Pepsi");
                                 break;
                             case 'C':
-                                System.out.println("You ordered Coke");
-                                break;
                             case 'c':
                                 System.out.println("You ordered Coke");
                                 break;
                             case 'S':
-                                System.out.println("You ordered Sprite");
-                                break;
                             case 's':
                                 System.out.println("You ordered Sprite");
                                 break;
                             case 'M':
-                                System.out.println("You ordered Mountain Dew");
-                                break;
                             case 'm':
                                 System.out.println("You ordered Mountain Dew");
                                 break;
                         }
                     }
                     break;
-                
                 // Choice of fries 
                 case 'F':
-                    System.out.println(fries);
-                    friesChoice = scanner.next();
-                    if (!(friesChoice.length() > 1)) {
-                        switch (friesChoice.charAt(0)) {
-                            case 'L':
-                                System.out.println("You ordered large fries");
-                                break;
-                            case 'l':
-                                System.out.println("You ordered large fries");
-                                break;
-                            case 'S':
-                                System.out.println("You ordered small fries");
-                                break;
-                            case 's':
-                                System.out.println("You ordered small fries");
-                                break;
-                        }
-                    }
-                    break;
-                    
                 case 'f':
                     System.out.println(fries);
                     friesChoice = scanner.next();
                     if (!(friesChoice.length() > 1)) {
                         switch (friesChoice.charAt(0)) {
                             case 'L':
-                                System.out.println("You ordered large fries");
-                                break;
                             case 'l':
                                 System.out.println("You ordered large fries");
                                 break;
                             case 'S':
-                                System.out.println("You ordered small fries");
-                                break;
                             case 's':
                                 System.out.println("You ordered small fries");
                                 break;
                         }
                     }
                     break;
-                
                 // If the user enters a character that is not valid, the following String will be printed
                 default:
                     System.out.println("You did not enter any of B, b, S, s, F, or f");
 
             }
         }
-        
         // if the user enters a String longer than one character, the following String will be printed
         else {
             System.out.println("A single character expected");
