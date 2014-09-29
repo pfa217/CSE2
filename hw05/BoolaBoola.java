8982fa37d3fc159933f426523cebf3e9ff7ad009
Pablo Felipe Aviles Ruiz <pfa217@lehigh.edu>
2014-09-29 14:33:45 +0000

    Updated comments

--- hw05/BoolaBoola.java
+++ hw05/BoolaBoola.java
@@ -1,3 +1,8 @@
+//// Pablo Aviles
+// Lab 04
+// Boola boola
+// The program assigns three random boolean values and asks the user to perform a logic operation. If the operation is correct, a correct message will be displayed.
+// if it is incorrect, a try again message will be displayed
 
 import java.util.Scanner;
 
@@ -6,19 +11,20 @@ public class BoolaBoola {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        boolean a = Math.random() < 0.5;
+        
+        boolean a = Math.random() < 0.5; // If the number returned is less than 0.5, variable will be initiated as true, if its more than 0.5, it will be initiated as false
         boolean b = Math.random() < 0.5;
         boolean c = Math.random() < 0.5;
         boolean andOr1 = Math.random() < 0.5;
         boolean andOr2 = Math.random() < 0.5; // if true operator will be &&, if false operator will be or
         String response = "";
+        
 
-        if (andOr1) {
-            if (andOr2) {
+        if (andOr1) {   // If statement for first operator True will be &&
+            if (andOr2) { // second operator as and
                 System.out.print("Does " + a + " && " + b + " && " + c + " have the value of true(t/T) or false(f/F): ");
                 response = scanner.next();
-                if ((a && b || c)) {
+                if ((a && b || c)) { 
                     if (response.equals("T") || response.equals("t")) {
                         System.out.println("Correct!");
                     } else {
@@ -31,7 +37,7 @@ public class BoolaBoola {
                         System.out.println("Wrong, try again");
                     }
                 }
-            } else {
+            } else { // Second operator as or
                 System.out.print("Does " + a + " && " + b + " || " + c + " have the value of true(t/T) or false(f/F): ");
                 response = scanner.next();
                 if ((a && b || c)) {
@@ -48,8 +54,8 @@ public class BoolaBoola {
                     }
                 }
             }
-        } else {
-            if (andOr2) {
+        } else { // Else will make first operator ||
+            if (andOr2) { // second operator &&
                 System.out.print("Does " + a + " || " + b + " && " + c + " have the value of true(t/T) or false(f/F): ");
                 response = scanner.next();
                 if ((a || b && c)) {
@@ -65,7 +71,7 @@ public class BoolaBoola {
                         System.out.println("Wrong, try again");
                     }
                 }
-            } else {
+            } else { // second operator ||
                 System.out.println("Does " + a + " || " + b + " || " + c + " have the value of true(t/T) or false(f/F): ");
                 response = scanner.next();
                 if ((a || b || c)) {
