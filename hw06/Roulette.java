88f18ca26ee7f0d6920e853b1d8f74ea3a8dd0d3
Pablo Felipe Aviles Ruiz <pfa217@lehigh.edu>
2014-10-15 00:04:42 +0000

    Updated Roulette

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -14,38 +14,31 @@ public class Roulette {
         int numberComesUp = 0;
         int spins;
         int rouletteNum = 0;
-        int moneyLeft = 100;
         int moneyEarned = 0;
         int timesWalkedAwayWMoney = 0;
-        int timesLostEverything = 0;
+        int timesLost = 0;
 
             for (int j = 0; j < 1000; j++) { // User plays 1000 times
                 for (spins = 0; spins < 100; spins++) { // user spins 100 times 
                     if (numberChosen == (int) (1 + Math.random() * 38)) { // if the user chooses the right number, numberComesUp is added 1
                         numberComesUp++;
                     }
-                    moneyLeft--;
-                    if(moneyLeft == 0){ // if the user is out of money, the loop is broken
-                        break;
-                    }
                 }
                 if (numberComesUp >= 3) { // if the number came up 3 or more times the user earns the money
-                    moneyEarned = numberComesUp*36;
+                    moneyEarned += numberComesUp*36;
                     timesWalkedAwayWMoney++;
-                    moneyLeft += moneyEarned;
-                } else { // if not, the user loses those bets
-                    timesLostEverything++;
+                    //moneyLeft += moneyEarned;
+                } else { // if not, the user lose money
+                    timesLost++;
+                    moneyEarned += numberComesUp*36;
                 }
                 numberComesUp =0;
-                if (moneyLeft == 0) {
-                    break;
-                }
             }
         
         // output
         System.out.println("Total Earnings: " + moneyEarned + "\n" +
                             "Times you walked away with money: " +timesWalkedAwayWMoney +"\n" +
-                            "Times you lost everything: "+timesLostEverything);
+                            "Times you lost money: "+timesLost);
 
     }
 
